8876259d0dc7ed749ae2035e38fe5254c10fa7f2
edwinlomolo <workockmoses@gmail.com>
2024-05-14 00:04:28 +0300

    cleanup

--- courier-mobile/app/src/main/java/com/lomolo/courier/compose/screens/home/Home.kt
+++ courier-mobile/app/src/main/java/com/lomolo/courier/compose/screens/home/Home.kt
@@ -5,7 +5,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapProperties
